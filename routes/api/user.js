112132c5422029e6f3cec5cc71106c48cf5c2378
CuelloBrisa <brisacuello.a@gmail.com>
2020-06-22 17:20:56 -0300

    segundo commit

---
+++ routes/api/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../../models/User');
+
+//GET a api/users (todos los usuarios)
+router.get('/users', (req, res) => {
+    User.find ((err, users) => {
+        if (err) throw err;
+        res.status(200).json(users);
+    });
+});  
+
+//GET a api/user/id (un solo usuarios)
+router.get('/user/:id', (req, res) => {
+    User.findOne ({ id: req.params.id }, (err, user) => {
+        if (err) throw err;
+        res.status(200).json(user);
+    });
+});
+
+module.exports = router;
